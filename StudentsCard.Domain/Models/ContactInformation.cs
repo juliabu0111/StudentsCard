5c1deebedc99e885b33b7d5ca43e938de772d84c
Yullia Burka <jul1a.burka@email.ua>
2022-02-11 17:25:25 +0200

    Add Contact and Passport models

---
+++ StudentsCard.Domain/Models/ContactInformation.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using StudentsCard.Domain.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsCard.Domain.Models
+{
+    [CollectionName("Contact Information")]
+    public class ContactInformation : BaseModel, ICloneable
+    {
+        [RegularExpression(@"\d{5}*$", ErrorMessage = "Invalid Zip Code")]
+        public string ZipCode { get; set; }
+
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]*$")]
+        public string Region { get; set; }
+
+        [RegularExpression(@"^[a-zA-Zа-яА-Я]*$")]
+        public string District { get; set; }
+
+        public string SettlementName { get; set; }
+
+        public string Street { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9]*$")]
+        public string HouseNumber { get; set; }
+
+        [RegularExpression(@"^[0-9]*$")]
+        public string ApartmentNumber { get; set; }
+
+        public string ResidenceAddress => $"{ SettlementName }, { Street }, { HouseNumber }, { ApartmentNumber }.";
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public object Clone()
+        {
+            return new ContactInformation()
+            {
+                Id = Id,
+                ZipCode = ZipCode,
+                Region = Region,
+                District = District,
+                SettlementName = SettlementName,
+                Street = Street,
+                HouseNumber = HouseNumber,
+                ApartmentNumber = ApartmentNumber,
+                PhoneNumber = PhoneNumber,
+                Email = Email
+            };
+        }
+    }
+}
---
+++ StudentsCard.Domain/Models/PassportInformation.cs
@@ -0,0 +1,42 @@
+﻿using StudentsCard.Domain.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsCard.Domain.Models
+{
+    [CollectionName("Passport Information")]
+    public class PassportInformation : BaseModel, ICloneable
+    {
+        [Required(ErrorMessage = "Passport series is required.")]
+        [RegularExpression(@"^[A-ZА-Я]{2}*$", ErrorMessage = "Only alphabets should be entered")]
+        public string Series { get; set; }
+
+        [Required(ErrorMessage = "Passport number is required.")]
+        [RegularExpression(@"^[0-9]{6}*$", ErrorMessage = "Only 6 numbers should be entered")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "Date of issue is required.")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: dd/MM/yyyy}")]
+        public DateTime DateOfIssue { get; set; }
+        
+        [Required(ErrorMessage = "Authority is required.")]
+        public string Authority { get; set; }
+
+        [Required(ErrorMessage = "Nationality is required.")]
+        public string Nationality { get; set; }
+
+        public string CivilStatus { get; set; }
+
+        public object Clone()
+        {
+            return new PassportInformation()
+            {
+                Series = Series,
+                Number = Number,
+                DateOfIssue = DateOfIssue,
+                Authority = Authority,
+                Nationality = Nationality,
+                CivilStatus = CivilStatus
+            };
+        }
+    }
+}
