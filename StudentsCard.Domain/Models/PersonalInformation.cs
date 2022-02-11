244921cef2a57d6ed349fab4688639113cf7e52c
Yullia Burka <jul1a.burka@email.ua>
2022-02-11 17:24:56 +0200

    Did changes in the PersonalInf model

--- StudentsCard.Domain/Models/PersonalInformation.cs
+++ StudentsCard.Domain/Models/PersonalInformation.cs
@@ -7,23 +7,20 @@ namespace StudentsCard.Domain.Models
     [CollectionName("Personal Information")]
     public class PersonalInformation : BaseModel, ICloneable
     {
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(20)]
         public string LastName { get; set; }
 
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(15)]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "Middle name is required.")]
+        [MaxLength(20)]
         public string MiddleName { get; set; }
 
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: dd/MM/yyyy}")]
         public DateTime Birthday { get; set; }
-
-        public string Citizenship { get; set; }
-
-        public string CivilStatus { get; set; }
 
         [JsonIgnore]
         public string FullName => $"{ LastName } { FirstName } { MiddleName }";
@@ -39,9 +36,7 @@ namespace StudentsCard.Domain.Models
                 LastName = LastName,
                 FirstName = FirstName,
                 MiddleName = MiddleName,
-                Birthday = Birthday,
-                Citizenship = Citizenship,
-                CivilStatus = CivilStatus
+                Birthday = Birthday
             };
         }
     }
